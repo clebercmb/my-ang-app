23909120b9e638e3bc60e5882c990672c6bb06f5
Cleber Miranda Barbosa <cleber@gmail.com>
2021-08-14 10:32:37 -0400

    second commit

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,38 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-reactiveform',
   templateUrl: './reactiveform.component.html',
-  styleUrls: ['./reactiveform.component.css'],
+  styleUrls: ['./reactiveform.component.scss'],
 })
 export class ReactiveformComponent implements OnInit {
-  myform: FormGroup;
+  myForm: FormGroup;
   uname: FormControl;
   email: FormControl;
   city: FormControl;
   password: FormControl;
 
   createForm() {
-    this.myform = new FormGroup({
+    this.myForm = new FormGroup({
       uname: this.uname,
       email: this.email,
       password: this.password,
       city: this.city,
     });
   }
+
   createFormControls() {
     this.uname = new FormControl('');
     this.email = new FormControl('');
     this.password = new FormControl('');
     this.city = new FormControl('');
   }
+
   constructor() {}
 
   ngOnInit(): void {
     this.createFormControls();
     this.createForm();
   }
+
   addUser() {
-    console.log(this.myform.value);
+    //alert('user is added', this.myForm.value);
   }
 }
